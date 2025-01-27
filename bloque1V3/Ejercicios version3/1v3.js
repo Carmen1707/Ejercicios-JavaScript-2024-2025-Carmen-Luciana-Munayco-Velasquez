0dbe9181b1e14cf6a3c4a24cd8c2e0ed5ac88061
Carmen1707 <cmunvel574@g.educaand.es>
2025-01-27 12:17:19 +0100

    10

---
+++ bloque1V3/Ejercicios version3/1v3.js
@@ -0,0 +1,6 @@
+let nombre;
+let edad;
+nombre=prompt("Introducir tu nombre: ");
+edad=prompt("Introducir tu edad: ");
+document.write("Tu nombre es :" +nombre);
+document.write("<br>Tu edad es :" +edad );
